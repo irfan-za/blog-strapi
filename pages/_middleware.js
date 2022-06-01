bdc17eba9ae2b7c891b0005631a20310b4263ad0
irfan-za <fanzahran123@gmail.com>
2022-06-01 20:36:56 +0700

    remove middleware

--- pages/_middleware.js
+++
@@ -1,9 +0,0 @@
-import {NextResponse} from "next/server"
-
-export default function middleware(req){
-  const url= req.nextUrl.clone()
-
-  if(url.pathname==='/admin'){
-    return NextResponse.rewrite("http://localhost:3000/detail")
-  }
-}
